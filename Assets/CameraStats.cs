af0de6ede4778af67ccf902ed05ef6884888caea
Strialck <82585816+Strialck@users.noreply.github.com>
2021-11-09 14:01:57 +0300

    Мелкие правки

--- Assets/CameraStats.cs
+++ Assets/CameraStats.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraStats
 {
     [SerializeField]
-    private Camera Camera;
+    private Camera _camera;
     public float CamWidth { get; private set; }
     public float CamHeght { get; private set; }
     public float MaxX { get; private set; }
@@ -22,10 +22,10 @@ public class CameraStats
     private CameraStats()
     {
         Border = 0.1f;
-        this.Camera = Camera.main;
-        _camPosition = Camera.transform.position;
-        CamHeght = Camera.orthographicSize;
-        CamWidth = CamHeght * Camera.aspect;
+        this._camera = Camera.main;
+        _camPosition = _camera.transform.position;
+        CamHeght = _camera.orthographicSize;
+        CamWidth = CamHeght * _camera.aspect;
         MaxX = _camPosition.x + CamWidth;
         MinX = _camPosition.x - CamWidth;
         MaxY = _camPosition.y + CamHeght;
@@ -33,21 +33,7 @@ public class CameraStats
         CamHeght *= 2;
         CamWidth *= 2;
     }
-    public static Vector3 GetRandomPosOnCamBorder()
-    {
-        Vector3 position = new Vector3();
-        if (Random.Range(0f, 1f) > 0.5f) 
-        {
-            position.x = Random.Range(0f, 1f) > 0.5f ? GetInstance().MaxX - GetInstance().Border : GetInstance().MinX + GetInstance().Border;
-            position.y = Random.Range(GetInstance().MinY + GetInstance().Border, GetInstance().MaxY - GetInstance().Border);
-        }
-        else
-        {
-            position.y = Random.Range(0f, 1f) > 0.5f ? GetInstance().MaxY - GetInstance().Border : GetInstance().MinY + GetInstance().Border;
-            position.x = Random.Range(GetInstance().MinX + GetInstance().Border, GetInstance().MaxX - GetInstance().Border);
-        }
-        return position;
-    }
+   
     public static CameraStats GetInstance()
     {
         if (_instance == null)
